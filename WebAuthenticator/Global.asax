30de0632446e1d1bdb7d4f28cddefb1ff352a206
lyvanhieu1997 <n15dcat007@student.ptithcm.edu.vn>
2019-11-06 23:20:53 +0700

    update ASPNETFRAMEWORK

---
+++ WebAuthenticator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAuthenticator.MvcApplication" Language="C#" %>
---
+++ WebAuthenticator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAuthenticator.Startup))]
+namespace WebAuthenticator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
